c36b345143ef3a9ab4029a0486f4d90c9e35f5a5
chirzin633 <chirzin.attabiq@gmail.com>
2026-09-14 22:43:25 +0700

    Homepage: create first homepage

---
+++ resources/views/components/category-nav.blade.php
@@ -0,0 +1,23 @@
+<ul class="me-auto mb-2 mb-lg-0 navbar-nav">
+    <li class="nav-item dropdown kategori-dropdown">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Kategori
+        </a>
+        <div class="dropdown-menu">
+            @foreach ($categories as $chunk)
+                <ul>
+                    @foreach ($chunk as $category)
+                        <li>
+                            <a
+                                class="dropdown-item"
+                                href="{{ route('categories.show', $category->slug) }}"
+                                >{{ $category->title }}</a
+                            >
+                        </li>
+                    @endforeach
+                </ul>
+            @endforeach
+        </div>
+    </li>
+    <li class="nav-item"><a class="text-white nav-link" href="{{ route('movies.index') }}">Movie</a></li>
+</ul>
