dc28f9d9f18bd411b3c919e5b2eb94af8c55e9bb
chirzin633 <chirzin.attabiq@gmail.com>
2026-09-14 22:03:15 +0700

    Homepage: relation category movie

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,9 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Category extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
+
+    protected $fillable = ['title', 'slug'];
+
+    public function movies(): BelongsToMany
+    {
+        return $this->belongsToMany(Movie::class);
+    }
 }
--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Movie extends Model
 {
@@ -28,4 +29,9 @@ class Movie extends Model
     protected $casts = [
         'release_date' => 'date'
     ];
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
