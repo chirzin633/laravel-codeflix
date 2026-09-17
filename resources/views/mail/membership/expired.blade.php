b6599c98c86b8286ab83856b2be2e99a9cce7540
chirzin633 <chirzin.attabiq@gmail.com>
2026-09-17 11:59:49 +0700

    Membership mailer

---
+++ resources/views/mail/membership/expired.blade.php
@@ -0,0 +1,8 @@
+<x-mail::message>
+    # Hello! Your membership has expired. Expired Date: {{ $expiredDate }}
+
+    <x-mail::button :url="$renewUrl"> Renew Membership </x-mail::button>
+
+    Thanks,<br />
+    {{ config('app.name') }}
+</x-mail::message>
